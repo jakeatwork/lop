6714348aa428a836184bb46f0ead099fb3e36f21
Jake Levine <jakelevine@livenation.com>
2012-01-12 17:51:43 -0800

    Finished Rails Env Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lop::Application.config.secret_token = 'e1ed821a27cb85a50ef15a2d0ca61664cb974e34ec26c4ce31013e4e1a5946403291f269c02f2450250cb9a26c171ed59320e478f6a6dcbf9c0d82c7628f1358'
